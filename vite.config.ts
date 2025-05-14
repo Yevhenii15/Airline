577d7385563c59adcbd86626b65ad200a21f9980
Yevhenii <yevmot01@easv365.dk>
2025-05-14 14:13:56 +0200

    try deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Yevhenii15.github.io/",
+  base: "/yevhenii15.github.io/",
 
   plugins: [vue(), vueDevTools()],
   resolve: {
