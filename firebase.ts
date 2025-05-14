953c3829186e5d8400b8e80654cc72ef1e21e0af
Yevhenii <yevmot01@easv365.dk>
2025-05-14 14:51:39 +0200

    chaged deploy to firebase

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTGM9jMyjjOrjdaRmyvLCEyc5YIXPkcOc",
+  authDomain: "airline-54feb.firebaseapp.com",
+  projectId: "airline-54feb",
+  storageBucket: "airline-54feb.firebasestorage.app",
+  messagingSenderId: "136955037087",
+  appId: "1:136955037087:web:c522572fef8afaebaaa9aa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
